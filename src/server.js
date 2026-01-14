17b28b5bd2b0e975018da3a717dce7a6be415355
brunoogalvani <bthezolin@gmail.com>
2026-01-14 19:04:45 -0300

    fix: arrumando cors para nova url

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['https://pima-backend.vercel.app','http://localhost:5173']
 }));
 
 app.use('/api', climaRoutes);
